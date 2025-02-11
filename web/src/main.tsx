f3d2ee2158d5c796768585cf39a65fb3d5a122a3
Tron <congzhang@dh-data.com>
2025-02-11 12:16:50 +0800

    craete project

---
+++ web/src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// TODO: Move to .env.local
+const PUBLISHABLE_KEY = 'pk_test_bmVhcmJ5LW9zdHJpY2gtMS5jbGVyay5hY2NvdW50cy5kZXYk'
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env.local file')
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>,
+)
