5e8776d31d0d9f6396fe47d479267700343958a7
York <yorkli@dh-data.com>
2025-02-14 18:55:43 +0800

    merge:拉取代码

--- web/src/pages/home/WaitChat.tsx
+++ web/src/pages/home/WaitChat.tsx
@@ -1,13 +1,17 @@
-import ChatInterface from "./ChatInterface"
+import ChatInterface from './ChatInterface'
 
 const WaitChat = () => {
   return (
     <div className="w-[100%] h-[100%]">
-        <ChatInterface prompt={<div className="p-4">
-          <div >What can I help with?</div>
-        </div>} />
+      <ChatInterface
+        prompt={
+          <div className="p-4">
+            <div>What can I help with?</div>
+          </div>
+        }
+      />
     </div>
   )
- }
+}
 
 export default WaitChat
